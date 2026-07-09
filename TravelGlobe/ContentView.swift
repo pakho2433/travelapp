a664df63834d980313f1035e5f128a29ec8d8f06
pakho2433 <pakho2433@gmail.com>
2026-07-09 14:38:43 +0800

    Inline Google Earth WebView for reliable Xcode build

--- TravelGlobe/ContentView.swift
+++ TravelGlobe/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import WebKit
 
 enum EarthDisplayMode: String, CaseIterable, Identifiable {
     case googleEarth = "Google Earth"
@@ -331,6 +332,42 @@ struct ContentView: View {
                 selectedPlace = nil
             }
         }
+    }
+}
+
+private struct GoogleEarthWebView: UIViewRepresentable {
+    var focusPlace: TravelPlace?
+
+    func makeUIView(context: Context) -> WKWebView {
+        let configuration = WKWebViewConfiguration()
+        configuration.allowsInlineMediaPlayback = true
+        configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.isOpaque = false
+        webView.backgroundColor = .black
+        webView.scrollView.backgroundColor = .black
+        webView.scrollView.bounces = false
+        webView.allowsBackForwardNavigationGestures = true
+        webView.load(URLRequest(url: googleEarthURL(for: focusPlace)))
+        return webView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let newURL = googleEarthURL(for: focusPlace)
+        if webView.url?.absoluteString != newURL.absoluteString {
+            webView.load(URLRequest(url: newURL))
+        }
+    }
+
+    private func googleEarthURL(for place: TravelPlace?) -> URL {
+        if let place {
+            let query = "\(place.latitude),\(place.longitude)"
+                .addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+            return URL(string: "https://earth.google.com/web/search/\(query)") ?? URL(string: "https://earth.google.com/web/")!
+        }
+
+        return URL(string: "https://earth.google.com/web/")!
     }
 }
 
